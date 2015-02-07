7f2596670d420b3b34f939df6f123feee57be3fc
John Vorster <bluglasshead@gmail.com>
2015-02-07 20:15:55 +0000

    Updated this to add Templates as suggested in Unit 3 Lesson 1.3

--- hello_world.py
+++ hello_world.py
@@ -1,35 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/hello")
 def hello_world():
-  return "Hello World!"
+    return render_template('index.html')
+
 
 @app.route("/hello/<name>")
 def hello_person(name):
-  html = """
-    <h1>
-      Hello {}!
-    </h1>
-    <p>
-      Here is a picture of a kitten. Awww...
-    </p>
-    <img src="http://placekitten.com/g/200/300">
-  """ 
-  return html.format(name.title())
+    return render_template('user.html', name=format(name.title()))
 
+
+#Left this the same so I can see the difference between this option and templating shown above
 @app.route("/jedi/<name>/<surname>")
 def jedi(name, surname):
-  html = """
-  <h1>
-    May the force be with you {}{}
-  </h1>
-  <p>
-    "It is an honour to meet a Jedi of your race."
-  </p>
-  """
-  return html.format(surname[:3].title(),name[:2])
+    html = """
+    <h1>
+      May the force be with you {}{}
+    </h1>
+    <p>
+      "It is an honour to meet a Jedi of your race."
+    </p>
+    """
+    return html.format(surname[:3].title(), name[:2])
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
